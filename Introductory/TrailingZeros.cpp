efa047743ef4ade5f53bdaee37a9ffe0283d794a
Daniel Jackson <daniiel.cavallcante@gmail.com>
2021-12-24 16:03:48 -0300

    Add files via upload

---
+++ Introductory/TrailingZeros.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n, ans = 0;
+
+    cin >> n;
+ 
+    for(int i = 5; n / i >= 1; i *= 5)
+        ans += n / i;
+
+    cout << ans << endl;
+
+    return 0;
+}
